29ff020c1f563c4bc9993b09e9374b2df9214676
Surasak somboon <ato2bla@gmail.com>
2020-04-14 19:46:31 +0700

    update new challenge flex

--- 21DayChallenge/10 - Kanban Board/start.js
+++ 21DayChallenge/10 - Kanban Board/start.js
@@ -1,4 +1,42 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let draggingElem;
+
+  function onDragStart(){
+    draggingElem = this;
+    // console.log(this);
+  }
+
+  function onDrop(){
+    // console.log('Drop');
+    this.append(draggingElem);
+    draggingElem = null;
+  }
+  function onDragOver(){
+    event.preventDefault();
+  }
+  function onDragEnter(){
+    event.preventDefault();
+  }
+
+  function run(){
+    const taskElem = Array.from(document.querySelectorAll('.task'));
+    const dropZoneElem = Array.from(document.querySelectorAll('.drop-zone'));
+    // console.log(taskElem);
+    // console.log(dropZoneElem);
+
+    taskElem.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart',onDragStart);
+    });
+
+    dropZoneElem.forEach((dropZoneElem) => {
+      dropZoneElem.addEventListener('drop',onDrop); //## need 2event dragOver and dragEnter to wrok!!
+
+      dropZoneElem.addEventListener('dragover',onDragOver);
+      dropZoneElem.addEventListener('dragenter',onDragEnter);
+    });
+
+  }
+  run();
 
 })();
--- 21DayChallenge/11 - Text Reveal/start.js
+++ 21DayChallenge/11 - Text Reveal/start.js
@@ -1,4 +1,28 @@
 (() => {
   // เริ่มเขียนโค้ด
 
+  function onScroll(){
+    const sectionElems = Array.from(document.querySelectorAll('section'));
+
+    sectionElems.forEach((sectionElem) => {
+      const imgElem = sectionElem.querySelector('img');
+      const textElem = sectionElem.querySelector('.text');
+
+      const scrollPostition = window.pageYOffset;
+      // console.log(scrollPostition);
+      const revealPostion = imgElem.offsetTop + (imgElem.offsetHeight / 10);
+
+      if(scrollPostition >= revealPostion){
+        textElem.classList.add('reveal');
+      }
+
+    });
+
+  }
+
+  function run(){
+    document.addEventListener('scroll',onScroll);
+  }
+  run();
+
 })();
