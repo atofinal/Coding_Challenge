b1890c0ecb4abc71ee119405f6777e57fe173c5d
Surasak somboon <ato2bla@gmail.com>
2020-04-16 17:26:48 +0700

    challenge 21day_13 30day_8

--- 21DayChallenge/13 - JavaScript Weird Parts/start.js
+++ 21DayChallenge/13 - JavaScript Weird Parts/start.js
@@ -1,8 +1,47 @@
 (() => {
   // เริ่มเขียนโค้ด
 
-  // 1. NaN
-  // 2. Type Coercion
-  // 3. Interpreter & Compiler
-  // 4. Checking Object Type
+  // ### 1. NaN
+  if (NaN * 1 === NaN) {
+    console.log('Equal');
+  } else {
+    console.log('NaN === NaN , Not Equal');
+  }
+
+  const result = 1 / 'hello';
+  if (Number.isNaN(result)) {
+    console.log (' result = 1 / "hello" , Equal to NaN');
+  }
+
+
+  // ### 2. Type Coercion
+  if (1 < 2 < 3){console.log('Inside, true > 1 = true');}
+  if (3 > 2 > 1){
+    console.log('Inside');
+  } else {
+    console.log(' 3 > 2 = true, true > 1 = false ');
+  }
+
+  console.log(2 - '1');
+  console.log(2 + '1');
+  console.log(2 + Number.parseInt('1',10));
+
+
+  // ### 3. Interpreter & Compiler
+  function getPerson(){
+    return {
+      name: 'surasak'
+    };
+  }
+  console.log(getPerson());
+
+
+  // ### 4. Checking Object Type
+  const person = {};
+  if (typeof person === 'object' && person !== null){
+    console.log('Yes, object');
+  }
+
+
+
 })();
