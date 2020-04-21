c6459056dd15e7feb3185d34dd234aa84c5689ee
Surasak somboon <ato2bla@gmail.com>
2020-04-21 12:17:46 +0700

    challenge 21day_18 30day_13

--- 21DayChallenge/18 - What is prototype/start.js
+++ 21DayChallenge/18 - What is prototype/start.js
@@ -1,8 +1,37 @@
 (() => {
   // เริ่มเขียนโค้ด
 
-  // 1. Class vs Prototype 
-  // 2. What's prototype?
-  // 3. Prototype chain
-  // 4. Extend a prototype
+  // ##  1. Class vs Prototype 
+  class Person {
+
+  }
+  const surasak = new Person();
+  console.log(surasak);
+
+
+  // ##  2. What's prototype?
+  const name = "Surasak";  
+  console.log(name.__proto__); // ## show method in proto
+  console.log(name);
+
+  const arr = [];
+  console.log(arr.__proto__);
+
+
+  // ##  3. Prototype chain
+  const name03 = "ato";
+  console.log(name03.__proto__);
+
+  console.log(name03.toLocaleString());
+
+
+  // ##  4. Extend a prototype
+  const name04 = "GOoD";
+  function sayHello(val){
+    console.log(`Hello ${val}`);
+  }
+  String.prototype.sayHello = sayHello;
+  console.log(name04.__proto__);
+  name04.sayHello('Earth');
+
 })();
