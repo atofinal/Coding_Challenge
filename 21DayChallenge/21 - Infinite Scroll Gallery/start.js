79232ca5a9f58547bf87d380eb7c6b9b976582e7
Surasak somboon <ato2bla@gmail.com>
2020-04-24 14:17:06 +0700

    chllenge 21day_21 30day_16

--- 21DayChallenge/21 - Infinite Scroll Gallery/start.js
+++ 21DayChallenge/21 - Infinite Scroll Gallery/start.js
@@ -1,3 +1,55 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const KEY = "dlnt7wvygSxMDEBUVsume08Ju_BSFkhnoSC8Cv5DRxg";
+  let page = 1;
+
+  const loaderElem = document.querySelector('.loader');
+
+  function showLoader(){
+    loaderElem.classList.add('visible');
+  }
+
+  function hideLoader(){
+    loaderElem.classList.remove('visible');
+  }
+
+  async function displayImages(){
+    showLoader();
+
+    const result = await fetch(`https://api.unsplash.com/photos/?client_id=${KEY}&page=${page}`);
+    const images = await result.json();
+    // console.log(images);
+
+    const galleryElem = document.querySelector('.gallery');
+
+    images.forEach(image => {
+      const imgElem = document.createElement('img');      
+      imgElem.src = image.urls.small;
+
+      galleryElem.appendChild(imgElem);      
+    });
+
+    hideLoader();
+
+    page += 1;
+
+  }
+
+  function onScroll(){
+    const {scrollTop,clientHeight,scrollHeight} = document.documentElement;
+    // console.log(scrollTop,clientHeight,scrollHeight);
+
+    if(scrollTop + clientHeight >= scrollHeight - 10){
+      displayImages();
+    }
+  }
+
+  function run(){
+    document.addEventListener('scroll',onScroll);
+    displayImages();
+
+  }
+
+  run();
+
 })();
